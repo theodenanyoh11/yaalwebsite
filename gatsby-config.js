375448c3548857d1cc97271c182aabc0ff82e833
theodenanyoh11 <59576386+theodenanyoh11@users.noreply.github.com>
2020-03-25 21:43:06 -0400

    build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
-const dotenv = require('dotenv')
-
-if (process.env.NODE_ENV !=='production') {
-  dotenv.config ()
-}
+require("dotenv") .config ({
+  path: '.env',
+})
 
 
 module.exports = {
@@ -38,8 +36,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `cmlokv6nn36t`,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
       },
     },
   
