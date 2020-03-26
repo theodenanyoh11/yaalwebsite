71dd6178650b4906db395c5f3cf0adddcee15365
theodenanyoh11 <59576386+theodenanyoh11@users.noreply.github.com>
2020-03-25 21:52:04 -0400

    change

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
       options: {
         spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
         accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+        
       },
     },
   
